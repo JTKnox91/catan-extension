649dda3796b2f34c7e5aee3cbdca83f4b681426c
JTKnox91 <jtknox91@gmail.com>
2022-09-28 00:28:00 -0400

    passing parsed actions from content to popup

--- content.js
+++ content.js
@@ -1,8 +1,63 @@
 // Must match popup.js
 const READ_LOGS_REQUEST = "CatanExtenstionPopupReadLogsRequest";
 
-// From colonist.io DOM. Subject to change.
+// Game log element ID from colonist.io (Subject to change).
 const GAME_LOG_ID = "game-log-text";
+
+const asAltText = (key) => `alt="${key}"`;
+
+// Resource image alt tags from colonist.io (Subject to change).
+const BRICK_KEY = "brick"
+const brickRegex = /alt="brick"/g;
+const GRAIN_KEY = "grain"
+const grainRegex = /alt="grain"/g;
+const LUMBER_KEY = "lumber";
+const lumberRegex = /alt="lumber"/g;
+const ORE_KEY = "ore";
+const oreRegex = /alt="ore"/g;
+const WOOL_KEY = "wool";
+const woolRegex = /alt="wool"/g;
+const UKNOWN_KEY = "card";
+const unknownRegex = /alt="card"/g;
+
+// Purchases image alt tags from colonist.io (subject to change).
+const CITY_KEY = "city";
+const DEVELOPMENT_KEY = "development card";
+const ROAD_KEY = "road";
+const SETTLEMENT_KEY = "settlement";
+
+// Action names defined for the extenstion.
+// The regexes are subject to change. The action names are only included for debugging purposes.
+const STARTING_RESOURCES_ACTION = "starting";
+const startingResourcesRegex = /(\w+) received starting resources/;
+const HARVEST_RESOURCES_ACTION = "harvest";
+const DISCARD_RESOURCES_ACTION = "discard";
+const BUY_CITY_ACTION = "buy_city";
+const BUY_DEVELOPMENT_ACTION = "buy_development";
+const BUY_ROAD_ACTION = "buy_road";
+const BUY_SETTLEMENT_ACTION = "buy_settlement";
+const TRADE_ACTION = "trade";
+const STEAL_ACTION = "steal";
+
+function parseAction(innerHTML) {
+  if (startingResourcesRegex.test(innerHTML)) {
+    const match = startingResourcesRegex.exec(innerHTML);
+    // Return an array because trades/steals will exist as 2 actions
+    return [{
+      action: STARTING_RESOURCES_ACTION,
+      player: match[1],
+      resources: {
+        [BRICK_KEY]: (innerHTML.match(brickRegex) || []).length,
+        [GRAIN_KEY]: (innerHTML.match(grainRegex) || []).length,
+        [LUMBER_KEY]: (innerHTML.match(lumberRegex) || []).length,
+        [ORE_KEY]: (innerHTML.match(oreRegex) || []).length,
+        [WOOL_KEY]: (innerHTML.match(woolRegex) || []).length,
+      }
+    }];
+  }
+  return [];
+};
+
 
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
@@ -12,7 +67,10 @@ chrome.runtime.onMessage.addListener(
     if (!!logsContainer) {
       sendResponse({
         success: true,
-        data: Array.from(logsContainer.children).map((child) => child.innerText),
+        data: Array.from(logsContainer.children).reduce(
+            (actions, child) => {
+              return actions.concat(parseAction(child.innerHTML))
+            },[]),
       });
     } else {
       sendResponse({
--- popup.js
+++ popup.js
@@ -4,7 +4,32 @@ const READ_LOGS_REQUEST = "CatanExtenstionPopupReadLogsRequest";
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, READ_LOGS_REQUEST, function(response) {
     if (response.success) {
-      document.getElementById("messages").innerText = response.data.join("\n");
+      const playerResourceCounts = new Map();
+      response.data.forEach((action) => {
+        if (!playerResourceCounts.has(action.player)) {
+          playerResourceCounts.set(action.player, new Map());
+        }
+        const player = playerResourceCounts.get(action.player);
+        Object.keys(action.resources).forEach((resourceKey) => {
+          const resourceDelta = action.resources[resourceKey];
+          if (player.has(resourceKey)) {
+            player.set(resourceKey, 0);
+          }
+          player.set(resourceKey, player.get(resourceKey) + resourceDelta);
+        });
+      });
+
+      const template = document.createElement('template');
+      playerResourceCounts.forEach((resourceCounts, player) => {
+        let playerHtmlString = `<div><h2>${player}</h2><ul>`;
+        resourceCounts.forEach((amount, resourceKey) => {
+          playerHtmlString += `<li>${resourceKey}: ${amount}</li>`
+        });
+        playerHtmlString += `</ul></div>`
+        template.innerHTML += playerHtmlString;
+      });
+
+      document.getElementById("messages").replaceChildren(...Array.from(template.content.childNodes));
     } else {
       document.getElementById("errors").innerText = response.error || "Something Went Wrong";
     }
